d5aa867fb531056acb03a100b9f13b2246164188
Sushmita Shrestha <sushmita@outcodesoftware.com>
2021-01-19 14:32:25 +0545

    minor fixes

--- bottomsheet/src/main/java/com/example/bottomsheet/BottomSheetCustomActivity.kt
+++ bottomsheet/src/main/java/com/example/bottomsheet/BottomSheetCustomActivity.kt
@@ -2,31 +2,28 @@ package com.example.bottomsheet
 
 import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.bottomsheet.databinding.ActivityMainBinding
 import com.google.android.material.bottomsheet.BottomSheetDialog
 
-object BottomSheetCustomActivity {
+object BottomSheetCustomActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
     lateinit var bottomSheetAdapter: BottomSheetAdapter
 
-
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(binding.root)
-//    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 
     open fun s(c: Context, message: String) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
     }
 
-    open fun showEditDialog(context:Context,labels: Array<String>, images: IntArray) {
-        binding = ActivityMainBinding.inflate(LayoutInflater.from(context))
-
+    open fun showEditDialog(context: Context, labels: Array<String>, images: IntArray) {
         val dialog = BottomSheetDialog(context)
 
         bottomSheetAdapter = BottomSheetAdapter()
