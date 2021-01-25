4a5272a5b45fef88db95d52d5418195811fc386c
Sushmita Shrestha <sushmita@outcodesoftware.com>
2021-01-25 11:48:45 +0545

    refactor

--- bottomsheet/src/main/java/com/example/bottomsheet/BottomSheetCustomActivity.kt
+++ bottomsheet/src/main/java/com/example/bottomsheet/BottomSheetCustomActivity.kt
@@ -1,13 +1,18 @@
 package com.example.bottomsheet
 
 import android.content.Context
+import android.content.Intent
 import android.util.AttributeSet
 import android.util.Log
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.withStyledAttributes
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.bottomsheet.databinding.ActivityMainBinding
-import com.example.bottomsheet.databinding.DialogBottomSheetBinding
+import com.example.bottomsheet.databinding.BottomSheetCustomViewBinding
+import com.example.bottomsheet.databinding.DialogBottomCustomViewBinding
+import com.example.bottomsheet.databinding.DialogBottonCustomViewBinding
 import com.google.android.material.bottomsheet.BottomSheetDialog
 
 
@@ -30,11 +35,11 @@ class BottomSheetCustomActivity @JvmOverloads constructor(
         context: Context, profileData: MutableList<BottomSheetModel>
     ) {
         val dialog = BottomSheetDialog(context)
-        val bindingSheet = DialogBottomSheetBinding.inflate(LayoutInflater.from(context),null,false)
+        val bindingSheet = DialogBottomCustomViewBinding.inflate(LayoutInflater.from(context),null,false)
         dialog.setContentView(bindingSheet.root)
 
         bottomSheetAdapter = BottomSheetAdapter(this)
-        bindingSheet.rvProfile .apply {
+        bindingSheet.rvProfile.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = bottomSheetAdapter
         }
