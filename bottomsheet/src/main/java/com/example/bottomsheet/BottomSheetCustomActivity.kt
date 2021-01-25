19fb5ae5cb7b355138dd2bdba92d1ef8d22480a9
Sushmita Shrestha <sushmita@outcodesoftware.com>
2021-01-25 11:24:44 +0545

    added layout

--- bottomsheet/src/main/java/com/example/bottomsheet/BottomSheetCustomActivity.kt
+++ bottomsheet/src/main/java/com/example/bottomsheet/BottomSheetCustomActivity.kt
@@ -2,26 +2,37 @@ package com.example.bottomsheet
 
 import android.content.Context
 import android.content.Intent
+import android.util.AttributeSet
 import android.util.Log
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.withStyledAttributes
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bottomsheet.databinding.ActivityMainBinding
+import com.example.bottomsheet.databinding.BottomSheetCustomViewBinding
 
 
 class BottomSheetCustomActivity @JvmOverloads constructor(
     context: Context,
-    listener: InterfaceListener = InterfaceListener.DEFAULT
-) : InterfaceListener {
-    lateinit var binding: ActivityMainBinding
-
+    listener: InterfaceListener = InterfaceListener.DEFAULT,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr), InterfaceListener {
+    var binding: ActivityMainBinding
     lateinit var bottomSheetAdapter: BottomSheetAdapter
 
+    init {
+        binding =
+            ActivityMainBinding.inflate(LayoutInflater.from(context), null, false)
+    }
+
+
     fun showEditDialog(
-        context: Context,
-        view: RecyclerView, profileData: MutableList<BottomSheetModel>
+        context: Context, profileData: MutableList<BottomSheetModel>
     ) {
         bottomSheetAdapter = BottomSheetAdapter(this)
-        view.apply {
+        binding.rvProfileData.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = bottomSheetAdapter
         }
